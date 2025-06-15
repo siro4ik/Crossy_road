2a7b175057d7318586aa7432c28304d24dd64743
siro4ik <deputatov0@mail.ru>
2025-06-15 16:36:06 +0300

    +

---
+++ my-crossy-road-gamenpm/src/main.js
@@ -0,0 +1,8 @@
+import * as THREE from "three";
+import { Renderer } from "./components/Renderer";
+import { Camera } from "./components/Camera";
+import { player } from "./components/Player";
+import "./style.css";
+
+const scene = new THREE.Scene();
+scene.add
