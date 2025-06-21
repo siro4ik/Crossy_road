143fd2707fd10f0409e373a558bba92c275349bb
Igor <deputatov0@mail.ru>
2025-06-21 20:52:05 +0300

    +

--- my-crossy-road-gamenpm/src/components/Map.js
+++ my-crossy-road-gamenpm/src/components/Map.js
@@ -6,6 +6,22 @@ export const map = new THREE.Group();
 export function initializeMap() {
   const grass = Grass(0);
   map.add(grass);
+  addRows();
+}
+
+export function addRows(){
+  metadata.forEach((rowData, index)=>{
+    const rowIndex = index + 1;
+    if (rowData.type === "forest"){
+      const row = Grass(rowIndex);
+
+      rowData.trees.forEach(({tileIndex,height})=>{
+        const three = Tree(tileIndex, height);
+        row.add(three);
+      });
+      map.add(row);
+    }
+  });
 }
 
 export const metadata = [
